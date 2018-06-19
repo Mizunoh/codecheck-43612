85520fa19ac7b3cfe112b2f4e951fcdd11c87d3a
mizuno.hiroko <mizuno.hiroko@tis.co.jp>
2018-06-19 18:43:13 +0900

    枠だけ

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,9 +2,34 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
+		if (args[0] == "encode") {
+			String output = encode(args[1]);
+			System.out.println(output);
+		} else if (args[0] == "decode") {
+			int output = decode(args[1]);
+			System.out.println(output);
+		} else if (args[0] == "align") {
+			String output = align(args[1]);
 			System.out.println(output);
 		}
+	}
+
+	//与えられた10進数の数値をアルファベット数字に変換
+	private static  String encode(String p){
+		int param = Integer.parseInt(p);
+
+		return null;
+	}
+
+	//与えられたアルファベット数字の値を10進数の数値に変換
+	private static  int decode(String p){
+		int param = Integer.parseInt(p);
+		return 0;
+	}
+
+	 //あるアルファベット数字に対して、足すとアルファベット数字の値が、すべてHになるような値に変換
+	private static  String align(String p){
+		int param = Integer.parseInt(p);
+		return null;
 	}
 }
