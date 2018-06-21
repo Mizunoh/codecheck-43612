a464b6da65e2d8ed626320508beadbf95f25dea4
mizuno.hiroko <mizuno.hiroko@tis.co.jp>
2018-06-21 15:47:23 +0900

    試し

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,18 +2,20 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		String sub = args[0].substring(0, args[0].indexOf(",")-1);
-		System.out.println(sub);
-		System.out.println(args[0].substring(args[0].indexOf(",")+2, args[0].length()-1));
-		if (sub == "encode") {
-			String output = encode(args[0].substring(args[0].indexOf(",")+2, args[0].length()-1));
-			System.out.println(output);
-		} else if (sub == "decode") {
-			int output = decode(args[1]);
-			System.out.println(output);
-		} else if (sub == "align") {
-			String output = align(args[1]);
-			System.out.println(output);
+		for (int i = 0, l = args.length; i < l; i++) {
+			String sub = args[i].substring(0, args[i].indexOf(",")-2);
+			System.out.println(sub);
+			System.out.println(args[i].substring(args[i].indexOf(",")+2, args[i].length()-1));
+			if (sub == "encode") {
+				String output = encode(args[0].substring(args[0].indexOf(",")+2, args[0].length()-1));
+				System.out.println(output);
+			} else if (sub == "decode") {
+				int output = decode(args[1]);
+				System.out.println(output);
+			} else if (sub == "align") {
+				String output = align(args[1]);
+				System.out.println(output);
+			}
 		}
 	}
 
