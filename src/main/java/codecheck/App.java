d8fb77ccb3db80ed51df22b2e537168d40363064
mizuno.hiroko <mizuno.hiroko@tis.co.jp>
2018-06-21 15:55:24 +0900

    試し

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,21 +2,21 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String sub = args[i].substring(0, args[i].indexOf(",")-2);
-			System.out.println(sub);
-			System.out.println(args[i].substring(args[i].indexOf(",")+2, args[i].length()-1));
-			if (sub == "encode") {
-				String output = encode(args[0].substring(args[0].indexOf(",")+2, args[0].length()-1));
+		//for (int i = 0, l = args.length; i < l; i++) {
+			//String sub = args[i].substring(0, args[i].indexOf(",")-2);
+			//System.out.println(sub);
+			//System.out.println(args[i].substring(args[i].indexOf(",")+2, args[i].length()-1));
+			if (args[0] == "encode") {
+				String output = encode(args[1]);
 				System.out.println(output);
-			} else if (sub == "decode") {
+			} else if (args[0] == "decode") {
 				int output = decode(args[1]);
 				System.out.println(output);
-			} else if (sub == "align") {
+			} else if (args[0] == "align") {
 				String output = align(args[1]);
 				System.out.println(output);
 			}
-		}
+		//}
 	}
 
 	//与えられた10進数の数値をアルファベット数字に変換
