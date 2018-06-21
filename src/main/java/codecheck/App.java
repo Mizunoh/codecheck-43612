dbfe5189ac4c9f9f789e58538bb65c64ad69fb95
mizuno.hiroko <mizuno.hiroko@tis.co.jp>
2018-06-21 16:24:24 +0900

    調査

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,34 +2,42 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		//for (int i = 0, l = args.length; i < l; i++) {
-			//String sub = args[i].substring(0, args[i].indexOf(",")-2);
-			System.out.println(args[0]);
-			System.out.println(args[1]);
-			String sub = args[0];
-			//System.out.println(args[i].substring(args[i].indexOf(",")+2, args[i].length()-String sub = arg));
-			if (sub.equals("encode")) {
+		//System.out.println(args[0]);
+		//System.out.println(args[1]);
+		String sub = args[0];
+		switch (sub) {
+			case "encode": {
 				String output = encode(args[1]);
 				System.out.println(output);
-			} else if (args[0] == "decode") {
+				break;
+			}
+			case "decode": {
 				int output = decode(args[1]);
 				System.out.println(output);
-			} else if (args[0] == "align") {
+				break;
+			}
+			case "align": {
 				String output = align(args[1]);
 				System.out.println(output);
+				break;
 			}
-		//}
+		}
 	}
 
 	//与えられた10進数の数値をアルファベット数字に変換
 	private static  String encode(String p){
-		//int param = Integer.parseInt(p);
-		System.out.println("encode in");
-		if (p.equals("777")){
-			return "BAFD";
-		}
-
-		return null;
+		int param = Integer.parseInt(p);
+		String result = Integer.toString(param, 8);
+		result.replace("0", "A")
+				.replace("1", "B")
+				.replace("2", "C")
+				.replace("3", "D")
+				.replace("4", "E")
+				.replace("5", "F")
+				.replace("6", "G")
+				.replace("7", "H")
+				.replace("8", "I");
+		return result;
 	}
 
 	//与えられたアルファベット数字の値を10進数の数値に変換
